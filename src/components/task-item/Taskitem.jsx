fd1fafcf381cf18ba9a04e7df97f1c18485d6775
Nguyen Vandev <nguyenhuuvan71198@gmail.com>
2022-07-22 15:09:29 +0700

    upload code

---
+++ src/components/task-item/Taskitem.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./taskitem.css";
+Taskitem.propTypes = {
+  taskName: PropTypes.string,
+  taskAuthor: PropTypes.string,
+  taskStatus: PropTypes.string,
+  taskDescription: PropTypes.string,
+};
+Taskitem.defaultProps = {
+  taskName: "Công việc",
+  taskAuthor: "Tên",
+  taskStatus: "New",
+  taskDescription: "Tập thể thao",
+};
+function Taskitem(props) {
+  const { taskName, taskAuthor, taskStatus, taskDescription } = props;
+  const [btnStatus, setBtnStatus] = useState("Start");
+  const [status, setStatus] = useState(taskStatus);
+  const [statusColor, setColor] = useState("#14ae5c");
+  function changeStatus() {
+    if (status === "New") {
+      setStatus("Doing");
+      setBtnStatus("Done");
+      setColor("#F88F14");
+    } else if (status === "Doing") {
+      setStatus("Done");
+      setBtnStatus("Renew");
+      setColor("#675BF1");
+    } else if (status === "Done") {
+      setStatus("New");
+      setBtnStatus("Start");
+      setColor("#14ae5c");
+    }
+  }
+  return (
+    <div class="col-3">
+      <div className="task-item">
+        <div>
+          <p class="strong">Title: {taskName}</p>
+          <p>Creator: {taskAuthor}</p>
+          <p class="strong" style={{ color: statusColor }}>Status: {status}</p>
+          <div className="task-description">
+            <p class="strong" >Description:</p>
+            <p>{taskDescription}</p>
+          </div>
+        </div>
+        <button className="status-btn strong" onClick={changeStatus}> {btnStatus} </button>
+      </div>
+    </div>
+  );
+}
+export default Taskitem;
